d027b58a81c15eea681f739f0dae4f71b142a532
Darren Stanford <darrenst@dracula.sens.buffalo.edu>
2017-04-10 19:03:16 -0400

    adding Problem2

--- Lecture7/fft_test_demonstrations.py
+++ Lecture7/fft_test_demonstrations.py
@@ -5,10 +5,11 @@ from fft import fft_power
 from numpy import array
 import math
 import time
+import numpy
 
 plotfirst = True
 
-if plotfirst == True : 
+if plotfirst == False : 
     # make some fake data as a single-frequency sinusoid
 
     N = 1024      # number of samples
@@ -16,8 +17,8 @@ if plotfirst == True :
     m = 0.00      # linear slope, if desired
 
     # For demonstrations : 
-    clip = 0      # "clip" so we can pad with zeros
-    window = False # window or not? 
+    clip = 200      # "clip" so we can pad with zeros
+    window = True # window or not? 
     
     
     x = array([ float(i) for i in xrange(N) ] )
@@ -63,53 +64,79 @@ else :
 
     window = False
 
-    yinput = []
-    xinput = []
+    yinput = []  #data === interpolated/avg/trend
+    xinput = []  #dates
 
     for line in lines :
         if line[0] != '#' :
             try:
-                words = line.split()
-                xval = float(words[2])
-                yval = float( words[4] )
-                yinput.append( yval )
-                xinput.append( xval )
+                year = int(line[0:4])
+                if year > 1957 and year < 2018:
+                    words = line.split()
+                    xval = float(words[2])  # dates
+                    yval = float( words[5] )# data 3=avg, 4=inter, 5=trend
+                    yinput.append( yval )
+                    xinput.append( xval )
             except ValueError :
                 print 'bad data:',line
+                
+#    yinput2 = yinput
+    yinput3 = []
+    xinput3 = []
 
 
+
+    for i in range(0,len(yinput)):
+        if yinput[i] > 0:
+            xinput3.append(xinput[i])
+            yinput3.append(yinput[i])
+
+
+
+N = len(yinput) # length of data
+log2N = math.log(N, 2)
+if log2N - int(log2N) > 0.0 :
+    print 'Padding with zeros!'
+    pads = [300.0] * (pow(2, int(log2N)+1) - N)
+    yinput = yinput + pads
     N = len(yinput)
-    log2N = math.log(N, 2)
-    if log2N - int(log2N) > 0.0 :
-        print 'Padding with zeros!'
-        pads = [300.0] * (pow(2, int(log2N)+1) - N)
-        yinput = yinput + pads
-        N = len(yinput)
-        print 'Padded : '
-        print len(yinput)
+    print 'Padded : '
+    print len(yinput)
         # Apply a window to reduce ringing from the 2^n cutoff
-        if window : 
-            for iy in xrange(len(yinput)) :
-                yinput[iy] = yinput[iy] * (0.5 - 0.5 * math.cos(2*math.pi*iy/float(N-1)))
+    if window : 
+        for iy in xrange(len(yinput)) :
+            yinput[iy] = yinput[iy] * (0.5 - 0.5 * math.cos(2*math.pi*iy/float(N-1)))
+
+
+
 
     y = array( yinput ) 
-    x = array([ float(i) for i in xrange(len(y)) ] )
+    x = array([ float(i) for i in xrange(len(y)) ])
     Y = fft(y)
+#    y2 = array( yinput2 ) 
+#    x2 = array( xinput )
+    y3 = array( yinput3 )
+    x3 = array( xinput3 )
 
     powery = fft_power(Y)
     powerx = array([ float(i) for i in xrange(len(powery)) ] )
 
     Yre = [math.sqrt(Y[i].real**2+Y[i].imag**2) for i in xrange(len(Y))]
 
+coefficents= numpy.polyfit(x3,y3,2)
+polynomial= numpy.poly1d(coefficents)
+print polynomial
 
-    plt.subplot(2, 1, 1)
-    plt.plot( x, y )
+#    plt.subplot(2, 1, 1)
+#    plt.plot( x3, y3 )
 
-    ax = plt.subplot(2, 1, 2)
-    p1, = plt.plot( powerx, powery )
-    p2, = plt.plot( x, Yre )
-    ax.legend( [p1, p2], ["Power", "Magnitude"] )
-    plt.yscale('log')
+#    ax = plt.subplot(2, 1, 2)
+#    p1, = plt.plot( powerx, powery )
+#    p2, = plt.plot( x, Yre )
+#    ax.legend( [p1, p2], ["Power", "Magnitude"] )
+#    plt.yscale('log')
 
 
-    plt.show()
+#    plt.show()
+
+

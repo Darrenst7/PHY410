2f0b7b1dd82a5339e38f645bd1bbf6ade1456caf
Darren Stanford <darrenst@dracula.sens.buffalo.edu>
2017-04-24 21:03:01 -0400

    adding quad code

--- Lecture10/quadrature.py
+++ Lecture10/quadrature.py
@@ -10,10 +10,14 @@ n2 = 0
 while n1 % 2 != 0 :
     n1 = int(raw_input( "Enter number of intervals desired for trapezoidal rule (must be even)" ))
 
+#a = 0
+#b = 2 * atan(1.0)
 a = 0
-b = 2 * atan(1.0)
-ans1 = trapezoid(sin, a, b, n1)
+b = 1.0
+ans1 = trapezoid(exp, a, b, n1)
 print 'Trapezoidal rule = ' + str(ans1)
+print 'Difference using the Trapezoidal method = ' + str(abs(ans1 - (exp(1)-1)))
 
-ans2 = adaptive_trapezoid(sin, a, b, 0.0001)
-print 'Adaptive trapezoidal rule = ' + str(ans2)
+ans2 = simpson(exp, a, b, n1)
+print 'Simpsons rule = ' + str(ans2)
+print 'Difference using the Simpsons method = ' + str(abs(ans2 - (exp(1)-1)))

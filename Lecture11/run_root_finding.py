1b9d3bcf687482c4d3201e5b51504b1bd7c91056
Darren Stanford <darrenst@dracula.sens.buffalo.edu>
2017-04-24 21:05:03 -0400

    adding root

--- Lecture11/run_root_finding.py
+++ Lecture11/run_root_finding.py
@@ -2,24 +2,26 @@ from root_finding import *
 from math import *
 
 def f ( x ) :
-    return exp(x) * log(x) - x * x
+#    return exp(x) * log(x) - x * x
+    return tanh(x)
 
-
-print(" Algorithms for root of exp(x)*log(x) - x*x")
+#print(" Algorithms for root of exp(x)*log(x) - x*x")
+print(" Algorithms for roots of tan(x)")
 print(" ------------------------------------------------")
 
-print(" 1. Simple search")
+print(" 1. Secant Method")
 x0 = float ( input(" Enter initial guess x_0 : ") )
-dx = float ( input(" Enter step dx : ") )
+x1 = float ( input(" Enter ending guess x_1 : ") )
 acc = float ( input(" Enter accuracy : ") )
-answer = root_simple(f, x0, dx, acc,1000,True)
+answer = root_secant(f, x0, x1, acc,1000,True)
 print  str ( answer ) + "\n\n"
 
 print(" 2. Bisection search")
-x0 = float ( input(" Enter bracketing guess x_0 : ") )
-x1 = float ( input(" Enter bracketing guess x_1 : ") )
-acc = float ( input(" Enter accuracy : ") )
+#x0 = float ( input(" Enter bracketing guess x_0 : ") )
+#x1 = float ( input(" Enter bracketing guess x_1 : ") )
+#acc = float ( input(" Enter accuracy : ") )
 answer = root_bisection(f, x0, x1, acc,1000,True)
 print  str ( answer ) + "\n\n"
+
 
 
